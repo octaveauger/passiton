e65286c9062b0dc93928b591c8d10ff51de8197b
Octave <octave.auger@gmail.com>
2015-07-22 23:49:41 +0100

    Finalised refresh token for Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
+  has_many :tokens, dependent: :destroy
+
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data["email"]).first
@@ -14,7 +16,15 @@ class User < ActiveRecord::Base
         provider: access_token.provider,
         uid: access_token.uid
       )
+      user.first_token(access_token)
     end
     user
+  end
+
+  # Set up the first access token once a user authorises the app
+  def first_token(access_token)
+    self.tokens.create(access_token: access_token.credentials.token,
+      refresh_token: access_token.credentials.refresh_token,
+      expires_at: Time.at(access_token.credentials.expires_at).to_datetime)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722192525) do
+ActiveRecord::Schema.define(version: 20150722195610) do
+
+  create_table "tokens", force: true do |t|
+    t.integer  "user_id"
+    t.string   "access_token"
+    t.string   "refresh_token"
+    t.datetime "expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tokens", ["user_id"], name: "index_tokens_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
