550ae34da6456929a8918b0430462cf1a4d521c6
Octave <octave.auger@gmail.com>
2015-07-22 20:27:30 +0100

    Fixed basic oauth for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    unless user
+      user = User.create(email: data["email"],
+        password: Devise.friendly_token[0,20],
+        provider: access_token.provider,
+        uid: access_token.uid
+      )
+    end
+    user
+  end
 end
