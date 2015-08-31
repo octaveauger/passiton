20b7497fb75624d28120e897357a87f539c7cfd6
Octave <octave.auger@gmail.com>
2015-08-31 22:38:31 +0100

    Added the possibility to add a guest user, e.g for auth request if they haven't signed up yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,13 @@ class User < ActiveRecord::Base
   has_many :requesters, through: :requested_authorisations
   has_many :granters, through: :granted_authorisations
 
+  # Manages the connection to Gmail and the User population
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"]).first
-    unless user
-      user = User.create(email: data["email"],
+    user = User.where(email: data["email"]).first
+    if user.nil?
+      user = User.create(
+        email: data["email"],
         password: Devise.friendly_token[0,20],
         provider: access_token.provider,
         uid: access_token.uid,
@@ -23,8 +25,32 @@ class User < ActiveRecord::Base
         last_name: data['last_name'],
         image: data['image'],
         gender: access_token.extra['raw_info']['gender'],
+        guest: false
       )
       user.first_token(access_token)
+    elsif user.guest
+      user.update!(
+        provider: access_token.provider,
+        uid: access_token.uid,
+        first_name: data['first_name'],
+        last_name: data['last_name'],
+        image: data['image'],
+        gender: access_token.extra['raw_info']['gender'],
+        guest: false
+      )
+    end
+    user
+  end
+
+  # Create an account for invites into the app
+  def self.create_guest(email)
+    user = User.where(email: email).first
+    unless user
+      user = User.create!(
+          email: email,
+          password: Devise.friendly_token[0,20],
+          guest: true
+          )
     end
     user
   end
