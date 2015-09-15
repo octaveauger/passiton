aa2c9f7875ca17b339d24d4f3aac105c757d4316
Octave <octave.auger@gmail.com>
2015-09-15 20:07:33 +0100

    Basic download and save attachment from gmail

--- app/models/message_attachment.rb
+++ app/models/message_attachment.rb
@@ -1,3 +1,20 @@
 class MessageAttachment < ActiveRecord::Base
   belongs_to :email_message
+
+  def download
+  	client = Gmail.new(self.email_message.email_thread.authorisation.granter.tokens.last.fresh_token)
+	attachment = client.download_attachment(self.email_message.messageId, self.attachmentId)
+	if attachment['data'].nil?
+		false
+	else
+		save_file(Base64.urlsafe_decode64(attachment['data']))
+		true
+	end
+  end
+
+  def save_file(attachment_content)
+  	File.open("app/assets/images/"+self.filename, 'wb') do |f|
+  		f.write(attachment_content)
+  	end
+  end
 end
