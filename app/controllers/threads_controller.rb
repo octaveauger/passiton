81e45c703c119cf7aad4226e92d23e85a6500abb
Amaury De Closset <adeclosset@gmail.com>
2015-11-07 14:15:17 +0000

    Shortened sender and recipients list on emails

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -1,13 +1,12 @@
 class ThreadsController < ApplicationController
   before_action :logged_in_user
 
-  layout 'stripped'
-
   def show
   	@thread = current_user.email_threads.where(synced: true).find_by(id: params['id'])
   	if !@thread.nil?
 	  	@emails = @thread.email_messages.includes(:message_attachments, :message_participants, :participants)
-	end
+	 end
+   render layout: !request.xhr?
   end
 
   def update_tags
