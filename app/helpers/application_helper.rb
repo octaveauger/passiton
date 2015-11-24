6dafb6cbc419669547315f28e5d3d72895d72bec
Octave <octave.auger@gmail.com>
2015-11-24 21:17:44 +0000

    Only show GA when on production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
 	opts[:end].nil? ? charEnd = string.length : charEnd = string[charStart..string.length].index(opts[:end]) + charStart - 1
     charStart.nil? or charEnd.nil? ? nil : string[charStart..charEnd]
   end
+
+  # Checks if we are on production (based on URL_HOST env variable, not Rails environment as staging also behaves as production for Rails)
+  def we_are_on_production
+  	ENV['URL_HOST'] == 'https://justpassiton.herokuapp.com'
+  end
+
 end
