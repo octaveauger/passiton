d5bea36ab0e30a043c6a3c998ccdf7887cb4d6be
Octave <octave.auger@gmail.com>
2015-11-26 23:49:09 +0000

    Fixing issues for requester

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -3,17 +3,27 @@ class ThreadsController < ApplicationController
 
   def show
   	thread_db = EmailThread.where(synced: true).find_by(id: params['id'])
-    if !thread_db.nil? and thread_db.authorisation.enabled and current_user == (thread_db.authorisation.granter or thread_db.authorisation.requester) and !(current_user == thread_db.authorisation.requester and thread_db.is_hidden?)
+    if !thread_db.nil? and thread_db.authorisation.enabled and [thread_db.authorisation.granter, thread_db.authorisation.requester].include? current_user and !(current_user == thread_db.authorisation.requester and thread_db.is_hidden?)
       @thread = thread_db
 	  	@emails = GmailSync.get_emails(@thread.authorisation, @thread.thread_id).sort_by { |e| e.internal_date }.reverse
       @message_attachments = @thread.message_attachments
-	 end
+    end
+    @debug = []
+    if thread_db.nil?
+      @debug.push('nil')
+    elsif !thread_db.authorisation.enabled
+      @debug.push('enabled')
+    elsif current_user != (thread_db.authorisation.granter or thread_db.authorisation.requester)
+      @debug.push('current')
+    elsif current_user == thread_db.authorisation.requester and thread_db.is_hidden?
+      @debug.push('hidden')
+    end
    render layout: !request.xhr?
   end
 
   def update_tags
   	thread = EmailThread.where(synced: true).find_by(id: params['thread_id'])
-    if !thread.nil? and thread.authorisation.enabled and current_user == (thread.authorisation.granter or thread.authorisation.requester)
+    if !thread.nil? and thread.authorisation.enabled and [thread.authorisation.granter, thread.authorisation.requester].include? current_user
   		if params[:tag_type] == 'highlight'
         if params[:tag_highlight] # Thread to be highlighted
     			thread.set_highlight(true)
