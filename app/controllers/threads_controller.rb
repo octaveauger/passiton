76fc3549df2e52666196b8c23e9972f8b89ec43b
Octave <octave.auger@gmail.com>
2015-11-27 01:12:45 +0000

    Fixed small bug + added tooltips to highlight / hide actions

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -3,7 +3,7 @@ class ThreadsController < ApplicationController
 
   def show
   	thread_db = EmailThread.where(synced: true).find_by(id: params['id'])
-    if !thread_db.nil? and thread_db.authorisation.enabled and [thread_db.authorisation.granter, thread_db.authorisation.requester].include? current_user and !(current_user == thread_db.authorisation.requester and thread_db.is_hidden?)
+    if !thread_db.nil? and !(!thread_db.authorisation.enabled and current_user == thread_db.authorisation.requester and current_user != thread_db.authorisation.granter) and [thread_db.authorisation.granter, thread_db.authorisation.requester].include? current_user and !(current_user == thread_db.authorisation.requester and thread_db.is_hidden?)
       @thread = thread_db
 	  	@emails = GmailSync.get_emails(@thread.authorisation, @thread.thread_id).sort_by { |e| e.internal_date }.reverse
       @message_attachments = @thread.message_attachments
@@ -13,7 +13,7 @@ class ThreadsController < ApplicationController
 
   def update_tags
   	thread = EmailThread.where(synced: true).find_by(id: params['thread_id'])
-    if !thread.nil? and thread.authorisation.enabled and [thread.authorisation.granter, thread.authorisation.requester].include? current_user
+    if !thread.nil? and !(!thread.authorisation.enabled and current_user == thread.authorisation.requester and current_user != thread.authorisation.granter) and [thread.authorisation.granter, thread.authorisation.requester].include? current_user
   		if params[:tag_type] == 'highlight'
         if params[:tag_highlight] # Thread to be highlighted
     			thread.set_highlight(true)
