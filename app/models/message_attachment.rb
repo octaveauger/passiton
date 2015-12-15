d13d59fc4b85aee008cd491130d7442abe1fae3a
Octave Auger <octave.auger@gmail.com>
2015-12-15 20:27:51 +0000

    Added sender email

--- app/models/message_attachment.rb
+++ app/models/message_attachment.rb
@@ -50,4 +50,10 @@ class MessageAttachment < ActiveRecord::Base
   def self.find_for_message(message_id)
     self.where('email_message_id = ?', message_id)
   end
+
+  # Returns the participant who sent that attachment
+  def participant_sender
+    message_participant = MessageParticipant.where(delivery: 'from').where(email_message_id: self.email_message_id).first
+    message_participant.nil? ? nil : message_participant.participant
+  end
 end
